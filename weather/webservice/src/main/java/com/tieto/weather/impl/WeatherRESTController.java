a89f407432a7bac078f115d8dbc60f9d116a667a
sutaakar <krlsuta@gmail.com>
2013-06-17 17:28:15 +0200

    HTTP basic authentication implemented SOAP auth. removed

--- weather/webservice/src/main/java/com/tieto/weather/impl/WeatherRESTController.java
+++ weather/webservice/src/main/java/com/tieto/weather/impl/WeatherRESTController.java
@@ -1,10 +1,6 @@
 package com.tieto.weather.impl;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
